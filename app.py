88db16ad5fce16c8fcf80dfbed0881d01103c4b2
Darshan Patil <darshanpatil05092001@gmail.com>
2023-12-12 17:30:12 +0530

    update

--- app.py
+++ app.py
@@ -131,5 +131,4 @@ def get_frames():
 if __name__ == '__main__':
     with app.app_context():
         create_database()
-        import uvicorn
-        uvicorn.run(app, host="127.0.0.1", port=5000, workers=4)
+        app.run(debug=True)
